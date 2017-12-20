7763fa56d1ff61b135ba306d3389c61306c4bb23
Armando Bracho <abracho@overgrounddata.com>
2017-12-20 10:10:17 +0000

    Initial commit to create the project

---
+++ GuessTheNumber.py
@@ -0,0 +1,11 @@
+""" This app lets the user try to guess a randomly generated number"""
+
+def title():
+    """
+        Function used to build the title of the app
+    """
+    print("--------------------------------------")
+    print("       GUESS THE NUMBER APP")
+    print("--------------------------------------")
+
+title()
