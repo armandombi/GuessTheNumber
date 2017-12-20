8b8b87b9c95601438dc892ff1cc279cdbf125cb6
Armando Bracho <abracho@overgrounddata.com>
2017-12-20 11:13:13 +0000

    Added functionality to play the guessing game

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,4 +1,5 @@
 """ This app lets the user try to guess a randomly generated number"""
+from random import randint
 
 def title():
     """
@@ -8,4 +9,26 @@ def title():
     print("       GUESS THE NUMBER APP")
     print("--------------------------------------")
 
-title()
+
+def lets_play_a_game(low, high):
+    """
+        Initiates a game of Guess the Number
+        :param low: the lowest value allowed for the user to guess
+        :param high: the highest value allowed for the user to guess
+    """
+    ran = randint(low, high)
+    inp = high+1
+    title()
+
+    while inp != ran:
+        inp = int(input("Guess a number between {} and {}: ".format(low, high)))
+        if inp == ran:
+            print("Yes!! {} was the number. Congratulations!".format(inp))
+        else:
+            if inp > ran:
+                print("Sorry but {} is Higher than the number".format(inp))
+            else:
+                print("Sorry but {} is Lower than the number".format(inp))
+
+
+lets_play_a_game(1, 100)
